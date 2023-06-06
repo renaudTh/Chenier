47ed7b7eaa2c2c638eb026845d12f26ca0cdc317
thomas.renaud <thomas.renaud@belledonne-communications.com>
2023-06-06 14:07:48 +0200

    Refactor battle

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #define MIN_VALUE 8
 #define MAX_VALUE 15
 
+typedef int PlayerId;
+
 typedef enum Family_t{
     Cubes = 0,
     Diamonds = 1,
@@ -29,9 +31,8 @@ Card * card_new(Family f, int value, bool visible){
 void card_flip(Card *c){
     c->visible = !c->visible;
 }
-Card * card_copy(const Card * c){
-    return card_new(c->f, c->value, c->visible);
-}
+Card *card_copy(const Card *c) { return card_new(c->f, c->value, c->visible); }
+void card_print(const Card *c) { printf("%02d ", c->value); }
 void card_destroy(Card * c){
     if(c){
         free(c);
@@ -118,15 +119,16 @@ void stack_print(const Stack * s){
 
     for(int i = 0; i < s->size; i++){
         Card * current = s->cards[i];
-        printf("%d-%d ",current->f, current->value);
+        card_print(current);
     }
-    printf("\n");
 }
 
 typedef struct BattleGame_t {
     int nb_players;
     Stack ** players;
     Stack ** game;
+    PlayerId *playing_id;
+    int nb_playing;
 } BattleGame;
 
 BattleGame * battle_game_new(int nb_players){
@@ -134,9 +136,12 @@ BattleGame * battle_game_new(int nb_players){
     b->nb_players = nb_players;
     b->players = malloc(nb_players * sizeof(Stack));
     b->game = malloc(sizeof(Stack) * nb_players);
+    b->playing_id = malloc(sizeof(PlayerId) * nb_players);
+    b->nb_playing = nb_players;
     for (int i = 0; i < nb_players; i++) {
         b->players[i] = stack_new_empty();
         b->game[i] = stack_new_empty();
+        b->playing_id[i] = i + 1;
     }
     return b;
 }
@@ -165,32 +170,36 @@ void battle_game_destroy(BattleGame *bg){
     free(bg);
 }
 
-int * battle_game_players_equals(BattleGame *bg){
-    int check[MAX_VALUE+1];
-    for(int i = 0; i <= MAX_VALUE; i++) check[i] = -1;
-    int * players_equals = calloc(bg->nb_players, sizeof(int));
-    for (int i = 0; i < bg->nb_players; i++)players_equals[i] = -1;
+bool battle_game_compute_playing_id(BattleGame *bg) {
+    int check[MAX_VALUE + 1] = {0};
+    int *players_equals = calloc(bg->nb_players, sizeof(int));
     int players_added = 0;
-    for(int i_player = 0; i_player < bg->nb_players; i_player++){
-        Stack * current = bg->game[i_player];
+    for (int i = 0; i < bg->nb_playing; i++) {
+        PlayerId id = bg->playing_id[i] - 1;
+        Stack *current = bg->game[id];
         Card * top = stack_read_first(current);
+        if (!top)
+            continue;
         int value = top->value;
-        if(check[value] == -1){
-            check[value] = i_player;
-        }
-        else if(players_added == 0){
+        if (check[value] == 0) {
+            check[value] = id + 1;
+        } else if (players_added == 0) {
             players_equals[players_added++] = check[value];
-            players_equals[players_added++] = i_player;
-        }
-        else{
-            players_equals[players_added++] = i_player;
+            players_equals[players_added++] = id + 1;
+        } else {
+            players_equals[players_added++] = id + 1;
         }
     }
     if(!players_added){
         free(players_equals);
-        return NULL;
+        return false;
+    } else {
+        free(bg->playing_id);
+        bg->playing_id = NULL;
+        bg->playing_id = players_equals;
+        bg->nb_playing = players_added;
+        return true;
     }
-    return players_equals;
 }
 
 bool battle_game_is_ended(BattleGame *bg){
@@ -201,72 +210,97 @@ bool battle_game_is_ended(BattleGame *bg){
     return false;
 }
 void battle_game_play_card(BattleGame *bg, bool visible){
-    for(int i = 0; i < bg->nb_players; i++){
-        if(stack_is_empty(bg->players[i])) continue;
-        Card *to_play = stack_pop(bg->players[i]);
+    for (int i = 0; i < bg->nb_playing; i++) {
+        PlayerId id = bg->playing_id[i] - 1;
+        if (stack_is_empty(bg->players[id]))
+            continue;
+        Card *to_play = stack_pop(bg->players[id]);
         to_play->visible = visible;
-        stack_add_card_on(bg->game[i], to_play);
+        stack_add_card_on(bg->game[id], to_play);
         //stack_print(bg->game[i]);
     }
 }
-int battle_game_get_winning_player(BattleGame *bg){
+PlayerId battle_game_get_winning_player(BattleGame *bg) {
     int max = 0;
-    int id_max = 0;
-    for(int i = 0; i < bg->nb_players; i++){
-        Card * top = stack_read_first(bg->game[i]);
+    PlayerId id_max = 0;
+    for (int i = 0; i < bg->nb_playing; i++) {
+        PlayerId id = bg->playing_id[i] - 1;
+        Card *top = stack_read_first(bg->game[id]);
+        if (!top)
+            continue;
         if(top->value > max){
             max = top->value;
-            id_max = i;
+            id_max = id;
         }
     }
     return id_max;
 }
-void battle_game_collect_cards_from_game(BattleGame *bg, int player_index){
-    for(int i = 0; i < bg->nb_players; i++){
-        stack_add_stack_on_bottom(bg->players[player_index], bg->game[i], false);
+void battle_game_collect_cards_from_game(BattleGame *bg, PlayerId winner_id) {
+    for (int i = 0; i < bg->nb_playing; i++) {
+        PlayerId id = bg->playing_id[i] - 1;
+        stack_add_stack_on_bottom(bg->players[winner_id], bg->game[id], false);
     }
 }
 void battle_game_print_game(BattleGame *bg){
     for(int i = 0; i < bg->nb_players; i++){
-        Card * c = stack_read_first(bg->game[i]);
-        printf("%d-%d | ", c->f, c->value);
+        stack_print(bg->game[i]);
+        printf(" | ");
         stack_print(bg->players[i]);
+        printf("\n");
     }
     printf("------------\n");
 }
-void battle_game_play_battle(BattleGame *bg, int * player_ids){
-    if(!player_ids){
+void battle_game_reset_playing_id(BattleGame *bg) {
+    free(bg->playing_id);
+    bg->playing_id = NULL;
+    bg->playing_id = malloc(sizeof(PlayerId) * bg->nb_players);
+    bg->nb_playing = 0;
+    for (int i = 0; i < bg->nb_players; i++) {
+        if (!stack_is_empty(bg->players[i])) {
+            bg->playing_id[bg->nb_playing++] = i + 1;
+        }
+    }
+}
+void battle_game_play_battle(BattleGame *bg, bool battle) {
+    if (!battle) {
         int wp = battle_game_get_winning_player(bg);
+        battle_game_reset_playing_id(bg);
+        battle_game_print_game(bg);
         battle_game_collect_cards_from_game(bg, wp);
         return;
     }
+    printf("Battle\n");
     battle_game_play_card(bg, false);
     battle_game_play_card(bg, true);
-
-
+    battle = battle_game_compute_playing_id(bg);
+    battle_game_play_battle(bg, battle);
 }
+
 void battle_game_main_loop(BattleGame *bg){
+    int wp = 0;
+    bool battle = false;
     while(!battle_game_is_ended(bg)){
 
         battle_game_play_card(bg, true);
-        battle_game_print_game(bg);
-        int *equals = battle_game_players_equals(bg);
-        
-        if(!equals){
-            int wp = battle_game_get_winning_player(bg);
-            battle_game_collect_cards_from_game(bg, wp);
-        }
-        else{
-            printf("Battle\n");
-            break;
-        }
+        battle = battle_game_compute_playing_id(bg);
 
+        if (!battle) {
+            wp = battle_game_get_winning_player(bg);
+            battle_game_print_game(bg);
+            battle_game_collect_cards_from_game(bg, wp);
+        } else {
+            battle_game_play_battle(bg, battle);
+        }
     }
 }
 
+// TODO :
+//  - When a player doesnt have any card in a battle;
+//  - When 3 players have a battle, the collect doesn't work properly
+//  - Detect a cycle for equality
+
 int main(){
     srand(time(NULL));
-
     int nb_players = 3;
     BattleGame *bg = battle_game_new(nb_players);
     battle_game_init(bg);
