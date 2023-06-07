bb9784c6809de62bda87de3236cc681fb99472aa
Thomas Renaud <thomas.renaud@belledonne-communications.com>
2023-06-07 17:40:04 +0200

    Write unit tests for Stack API

--- tester/battle-tester.c
+++
@@ -1,5 +0,0 @@
-
-#include <CUnit/Basic.h>
-#include <CUnit/CUnit.h>
-
-#include "battle/battle.h"
---
+++ tester/card-stack-tester.h
@@ -0,0 +1,6 @@
+#ifndef CARD_STACK_TESTER_H
+#define CARD_STACK_TESTER_H
+
+#include "stack.h"
+
+#endif
